7721549f8b9c48ff85fcf8be464c1c2e6b5d1da1
Stephan Hochhaus <stephan@yauh.de>
2014-10-24 11:26:22 +0200

    listing 2-9
    Adding a defined data set to the database upon server start

--- myFridge/myFridge.js
+++ myFridge/myFridge.js
@@ -15,7 +15,25 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    if (Meteor.isServer) {
+        Meteor.startup(function () {
+            // code to run on server at startup
+
+            // clear the data base if the server starts
+            Products.remove({});
+
+            // fill the database with some products
+            Products.insert({
+                name: 'Milk',
+                img: '/milk.png',
+                place: 'fridge'
+            });
+
+            Products.insert({
+                name: 'Bread',
+                img: '/bread.png',
+                place: 'supermarket'
+            });
+        });
+    }
 }
