b28b9334dee0f58fced8067973aae9eeff76aa4d
Stephan Hochhaus <stephan@yauh.de>
2015-02-27 12:55:12 +0100

    improved dragging code
    https://forums.manning.com/posts/list/35572.page#p99352

--- myFridge/myFridge.js
+++ myFridge/myFridge.js
@@ -1,40 +1,62 @@
 Products = new Meteor.Collection('products');
 
 if (Meteor.isClient) {
-    Template.fridge.helpers({
-        products: function(){
-            return Products.find({place: 'fridge'});
-        }
-    });
-    Template.productList.helpers({
-        products: function(){
-            return Products.find({place: 'supermarket'});
-        }
-    });
-    Template.fridge.rendered = function () {
-        $('#fridge').droppable({
-            drop: function (evt, ui) {
-                var query = {_id: $(ui.draggable).data('id')};
-                var data = {$set: {place: 'fridge'}};
-                Products.update(query, data);
-            }
-        });
-    };
-    Template.productList.rendered = function () {
-        $('#supermarket').droppable({
-        drop: function (evt, ui) {
-            var query = {_id: $(ui.draggable).data('id')};
-            var data = {$set: {place: 'supermarket'}};
-            Products.update(query, data);
-        }
-    });
+  Template.fridge.helpers({
+    products: function() {
+      return Products.find({
+        place: 'fridge'
+      });
+    }
+  });
+  Template.productList.helpers({
+    products: function() {
+      return Products.find({
+        place: 'supermarket'
+      });
+    }
+  });
+  Template.fridge.rendered = function() {
+    var templateInstance = this;
 
-    Template.productListItem.rendered = function () {
-        $(this.find('.draggable')).draggable({
-            cursor: 'move',
-            helper: 'clone'
-        });
-    };
+    templateInstance.$('#fridge').droppable({
+      drop: function(evt, ui) {
+        var query = {
+          _id: ui.draggable.data('id')
+        };
+        var changes = {
+          $set: {
+            place: 'fridge'
+          }
+        };
+        Products.update(query, changes);
+      }
+    });
+  };
+  Template.productList.rendered = function() {
+    var templateInstance = this;
+
+    templateInstance.$('#supermarket').droppable({
+      drop: function(evt, ui) {
+        var query = {
+          _id: ui.draggable.data('id')
+        };
+        var changes = {
+          $set: {
+            place: 'supermarket'
+          }
+        };
+        Products.update(query, changes);
+      }
+    });
+  };
+  Template.productListItem.rendered = function() {
+    var templateInstance = this;
+
+    templateInstance.$('.draggable').draggable({
+      cursor: 'move',
+      helper: 'clone'
+    });
+  };
 
 };
 
@@ -42,37 +64,37 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-    if (Meteor.isServer) {
-        Meteor.startup(function () {
-            // code to run on server at startup
+  if (Meteor.isServer) {
+    Meteor.startup(function() {
+      // code to run on server at startup
 
-            // clear the data base if the server starts
-            Products.remove({});
+      // clear the data base if the server starts
+      Products.remove({});
 
-            // fill the database with some products
-            Products.insert({
-                name: 'Milk',
-                img: '/milk.png',
-                place: 'fridge'
-            });
+      // fill the database with some products
+      Products.insert({
+        name: 'Milk',
+        img: '/milk.png',
+        place: 'fridge'
+      });
 
-            Products.insert({
-                name: 'Bread',
-                img: '/bread.png',
-                place: 'supermarket'
-            });
+      Products.insert({
+        name: 'Bread',
+        img: '/bread.png',
+        place: 'supermarket'
+      });
 
-            Products.insert({
-                name: 'Juice',
-                img: '/juice.png',
-                place: 'fridge'
-            });
+      Products.insert({
+        name: 'Juice',
+        img: '/juice.png',
+        place: 'fridge'
+      });
 
-            Products.insert({
-                name: 'Banana',
-                img: '/banana.png',
-                place: 'supermarket'
-            });
-        });
-    }
+      Products.insert({
+        name: 'Banana',
+        img: '/banana.png',
+        place: 'supermarket'
+      });
+    });
+  }
 }
